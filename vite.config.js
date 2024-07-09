6c3c5bfa6035b30a0d8f5ead53202c54af1ba64e
riyandarmawan <riyandarmawan042@gmail.com>
2024-07-09 14:23:16 +0700

    configure deploy

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/list-of-lit/',
   resolve: {
     alias: {
       "@components": path.resolve(__dirname, "src/components"),
